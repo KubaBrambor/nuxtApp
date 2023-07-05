a4aaf0c0b25440f83f49a301c0330f754f1519e5
Jakub Brambor <kuba.brambor@gmail.com>
2023-07-05 11:40:06 +0200

    Add handling route errors

--- nuxt-storm/pages/products/[id].vue
+++ nuxt-storm/pages/products/[id].vue
@@ -12,6 +12,9 @@
 
   const uri = 'https://fakestoreapi.com/products/' + id
   const { data: product } = await useFetch(uri, { key: id })
+  if(!product.value) {
+    throw createError({ statusCode: 404, statusMessage: 'Product not found', fatal: true })
+  }
 </script>
 
 <style scoped>
