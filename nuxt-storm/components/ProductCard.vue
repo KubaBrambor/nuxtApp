b3c993adbe5b9b3747d61fb6cf287649fe64936e
Jakub Brambor <kuba.brambor@gmail.com>
2023-07-05 11:28:29 +0200

    Add components

---
+++ nuxt-storm/components/ProductCard.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="card text-center flex flex-col justify-between">
+    <img :src="product.image" :alt="product.title" class="thumb h-[400px]">
+    <p class="font-bold text-gray-500 m-4 truncate">{{ product.title }}</p>
+    <NuxtLink :to="`/products/${product.id}`">
+      <p class="btn my-4">View Details</p>
+    </NuxtLink>
+  </div>
+</template>
+
+<script setup lang="ts">
+  const { product } = defineProps(['product'])
+</script>
+
+<style scoped>
+
+</style>
