e13a33a505b3cac40d21d63d857b9bf462e63022
Jakub Brambor <kuba.brambor@gmail.com>
2023-07-05 09:43:04 +0200

    Add nuxt-storm project and add nuxtlinks

---
+++ nuxt-storm/pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <header>
+      <nav>
+        <li><NuxtLink to="/">Home</NuxtLink></li>
+        <li><NuxtLink to="/about">About</NuxtLink></li>
+        <li><NuxtLink to="/products">Products</NuxtLink></li>
+      </nav>
+    </header>
+    <h1>Home Page</h1>
+    <h4>{{ text }}</h4>
+  </div>
+</template>
+
+<script setup lang="ts">
+  const text: string = 'Brainstorm session'
+</script>
+
+<style scoped>
+
+</style>
